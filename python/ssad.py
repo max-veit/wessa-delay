e1e3aa6bd61753535c12f760dbc662dfcbab10cb
Max Veit <maxdveit@gmail.com>
2014-01-21 17:22:42 -0600

    Code and documentation cleanup

--- python/ssad.py
+++ python/ssad.py
@@ -4,7 +4,6 @@ incorporating capability for delayed reactions.
 
 Classes:
     Reaction        Specification for an individual reaction pathway
-    Species         Unique identification for a chemical species
     Trajectory      Functionality to evolve a single trajectory in
                     configuration space
 
@@ -17,38 +16,38 @@ import numpy as np
 from numpy.random import exponential, random
 
 
-"""
-Encapsulation of information about a reaction pathway.
-
-Public methods:
-    calc_propensity     Given state information, calculate the propensity
-                        of this reaction pathway.
-
-"""
-
 class Reaction(object):
     """
-    Specify a reaction pathway.
+    Encapsulation of information about a reaction pathway.
 
-    Parameters:
-        reactants       Array of IDs (indices) of the reactants involved
-                        in this reaction
-        products        Array of indices of the products of this reaction
-        state_vec       State-change vector. Integer array of same length
-                        as total number of species, which, when added onto
-                        an existing state vector, gives the effect of this
-                        reaction.
-        propensity_const    A constant used in determining the propensity
-                        of this reaction. Different meaning for
-                        unimolecular, bimolecular, and generation
-                        reactions.
-    Optional Parameters:
-        delay           The delay, if any, associated with this reaction.
-                        Default 0.0.
+    Public methods:
+        calc_propensity     Given state information, calculate the propensity
+                            of this reaction pathway.
 
     """
+
     def __init__(self, reactants, products, state_vec,
                  propensity_const, delay=0.0):
+        """
+        Specify a reaction pathway.
+
+        Parameters:
+            reactants       Array of IDs (indices) of the reactants involved
+                            in this reaction
+            products        Array of indices of the products of this reaction
+            state_vec       State-change vector. Integer array of same length
+                            as total number of species, which, when added onto
+                            an existing state vector, gives the effect of this
+                            reaction.
+            propensity_const    A constant used in determining the propensity
+                            of this reaction. Different meaning for
+                            unimolecular, bimolecular, and generation
+                            reactions.
+        Optional Parameters:
+            delay           The delay, if any, associated with this reaction.
+                            Default 0.0.
+
+        """
         self.reactants = reactants
         self.products = products
         self.state_vec = np.asarray(state_vec)
@@ -58,13 +57,13 @@ class Reaction(object):
     # TODO
     # def read_list_from_file(filename):
 
-    """
-    Calculate the propensity for this reaction with the given
-    concentrations of the reactants. Currently only supports
-    unimolecular and bimolecular reactions.
-
-    """
     def calc_propensity(self, state):
+        """
+        Calculate the propensity for this reaction with the given
+        concentrations of the reactants. Currently only supports
+        unimolecular and bimolecular reactions.
+
+        """
         if len(self.reactants) == 0:
             return self.propensity_const
         elif len(self.reactants) == 1:
@@ -90,56 +89,35 @@ class Reaction(object):
                 " with rate " + str(self.propensity_const) +
                 " and delay " + str(self.delay))
 
-"""
-A Reaction overridden to always return a constant rate (useful in
-biological simulations)
 
-Public methods:
-    calc_propensity     Return this reaction's (fixed) rate or propensity.
-
-"""
-
-class ReactionConstRate(Reaction):
-    """Return this reaction's (fixed) propensity. State is ignored."""
-    def calc_propensity(self, state):
-        return self.propensity_const
-
-
-# TODO Not used - candidate for deletion
-# Replace with a dictionary or similar structure relating species to names.
-class Species(object):
-    def __init__(self, index, species_id):
-        self.index = index
-        self.species_id = species_id
-
+class StepsLimitException(Exception):
 """
 Exception to be raised when the simulation takes more steps than the
 pre-determined limit, if one exists.
 
 """
-class StepsLimitException(Exception):
     pass
 
 # TODO Document
 # TODO Implement sampling
 # TODO Test restart capability
 class Trajectory(object):
-    """
-    Initialize a new trajectory.
-
-    Parameters:
-        state       Initial state vector. Indices represent species
-                    indices, values are the populations of those species.
-        weight      Initial statistical weight of this trajectory.
-        reactions   List of reaction pathways that govern this trajectory's
-                    dynamics.
-
-    Optional Parameters:
-        init_time   Simulation time at which this trajectory starts.
-                    Defaults to 0.0 time units.
-
-    """
     def __init__(self, state, weight, reactions, init_time=0.0):
+        """
+        Initialize a new trajectory.
+
+        Parameters:
+            state       Initial state vector. Indices represent species
+                        indices, values are the populations of those species.
+            weight      Initial statistical weight of this trajectory.
+            reactions   List of reaction pathways that govern this trajectory's
+                        dynamics.
+
+        Optional Parameters:
+            init_time   Simulation time at which this trajectory starts.
+                        Defaults to 0.0 time units.
+
+        """
         self.state = np.asarray(state)
         self.weight = weight
         self.reactions = reactions
@@ -151,39 +129,40 @@ class Trajectory(object):
         self.next_rxn = None
         self.next_rxn_time = None
         self.next_rxn_delayed = False
+        self.rxn_tallies = defaultdict(lambda: 0)
 
-    """
-    Run the Gillespie SSA to evolve the initial concentrations in time.
-
-    If this method has been run on an object before, subsequent runs will
-    pick up where the last run left off. This means the effect of:
-        trj1.run_dynamics(duration1)
-        trj1.run_dynamics(duration2)
-    will be the same as that of
-        trj1.run_dynamics(duration1 + duration2)
-    ignoring possible pseudorandom number generator effects.
-    This capability is useful in, for example, weighted-ensemble
-    methods where the ability to pause and restart trajectories without
-    introducing any statistical bias is necessary.
-
-    Parameters:
-        duration    The amount of time for which the state should be
-                    evolved. The state of the trajectory will reflect
-                    that of the system after 'duration' units of time.
-
-    Optional Parameters:
-        max_steps   The maximum number of steps (reactions) that should be
-                    run. The trajectory will be evolved until the time runs
-                    out or the maximum number of steps is reached,
-                    whichever comes first. If the steps limit is reached
-                    first, the simulation time will be left at the time of
-                    the last reaction successfully executed and an
-                    exception will be raised.
-                    If set to None, this parameter will be ignored and the
-                    number of steps will be limited only by time.
-
-    """
     def run_dynamics(self, duration, max_steps=None):
+        """
+        Run the Gillespie SSA to evolve the initial concentrations in time.
+
+        If this method has been run on an object before, subsequent runs will
+        pick up where the last run left off. This means the effect of:
+            trj1.run_dynamics(duration1)
+            trj1.run_dynamics(duration2)
+        will be the same as that of
+            trj1.run_dynamics(duration1 + duration2)
+        ignoring possible pseudorandom number generator effects.
+        This capability is useful in, for example, weighted-ensemble
+        methods where the ability to pause and restart trajectories without
+        introducing any statistical bias is necessary.
+
+        Parameters:
+            duration    The amount of time for which the state should be
+                        evolved. The state of the trajectory will reflect
+                        that of the system after 'duration' units of time.
+
+        Optional Parameters:
+            max_steps   The maximum number of steps (reactions) that should be
+                        run. The trajectory will be evolved until the time runs
+                        out or the maximum number of steps is reached,
+                        whichever comes first. If the steps limit is reached
+                        first, the simulation time will be left at the time of
+                        the last reaction successfully executed and an
+                        exception will be raised.
+                        If set to None, this parameter will be ignored and the
+                        number of steps will be limited only by time.
+
+        """
         stop_time = self.time + duration
         if max_steps is not None:
             stop_steps = self.rxn_counter + max_steps
@@ -230,15 +209,15 @@ class Trajectory(object):
                         "Trajectory run reached maximum allowed number " +
                         "of steps (limit was " + str(max_steps) + ").")
 
-    """
-    Use random sampling to select the next reaction and firing time.
-
-    Returns a tuple of (reaction, wait time). The wait time is the duration
-    from the current simulation time to the time the next reaction fires.
-    It is sampled from an exponential distribution.
-
-    """
     def _sample_next_reaction(self):
+        """
+        Use random sampling to select the next reaction and firing time.
+
+        Returns a tuple of (reaction, wait time). The wait time is the duration
+        from the current simulation time to the time the next reaction fires.
+        It is sampled from an exponential distribution.
+
+        """
         for ridx, rxn in enumerate(self.reactions):
             self.propensities[ridx] = rxn.calc_propensity(self.state)
         prop_csum = np.cumsum(self.propensities)
@@ -251,16 +230,17 @@ class Trajectory(object):
                 break
         return (next_rxn, wait_time)
 
-    """Execute a reaction, along with hooks like recording the state."""
     def _execute_rxn(self, rxn, time):
+        """Execute a reaction, along with hooks like recording the state."""
         self.rxn_counter += 1
         self.state = self.state + rxn.state_vec
         self.time = time
         self.history[0].append(self.time)
         self.history[1].append(self.state)
+        self.rxn_tallies[rxn] += 1
 
-    """Save the trajectory state before pausing."""
     def _save_run_state(self, rxn, time, is_delayed):
+        """Save the trajectory state before pausing."""
         if is_delayed:
             heapq.heappush(self.event_queue, (time, rxn))
             self.next_rxn = rxn
@@ -270,6 +250,29 @@ class Trajectory(object):
             self.next_rxn = rxn
             self.next_rxn_time = time
             self.next_rxn_delayed = True
+
+    def sample_linspace(self, start_time, end_time, npoints):
+        """
+        Return a set of samples of the system state at linearly-spaced
+        intervals in time.
+
+        Parameters:
+            start_time      Simulation time at which the samples should start.
+            end_time        Simulation time at which samples should end.
+                            Both limits are inclusive.
+            npoints         Number of points at which to sample.
+
+        Returns:
+            A tuple (times, states) of NumPy arrays. The first array (1-D)
+            holds the times at which samples were taken, the second (2-D) holds
+            the state vectors with time along the first axis, species along the
+            second.
+
+        A warning is issued if start_time is less than the Trajectory's
+        init_time or if end_time is greater than the current time.
+
+        """
+        pass
 
     def __str__(self):
         msg = ("Trajectory at time " + str(self.time) +
