136dbc593b3a1346ad928c10519e1e3d4b932941
Max Veit <maxdveit@gmail.com>
2014-01-31 23:14:09 -0600

    More work on implementing weighted ensemble.
    Added Paving class, added methods but no implementation yet.

--- python/ensemble.py
+++ python/ensemble.py
@@ -8,10 +8,49 @@ about less-visited parts of the space.
 
 Classes:
     Ensemble    The base encapsulation of a weighted ensemble
-    Binning     Functionality related to binning of the phase space
+    Paving      Functionality related to binning of the phase space
 
 """
 
 import numpy as np
 
 
+class Ensemble(object):
+    pass
+
+
+# TODO Make abstract class?
+class Paving(object):
+    """
+    Functionality associated with a paving of phase space.
+
+    Public methods:
+        get_bin_num     Return the bin index for a given state
+
+    """
+
+    def __init__(self):
+        raise NotImplementedError("Abstract class.")
+
+    def get_bin_num(self):
+        raise NotImplementedError("Abstract class.")
+
+
+class UniformPaving(Paving):
+    """
+    A paving of phase space that is uniform in every dimension.
+
+    The binning is delimited by a rectangular region in phase space.
+    Coordinates outside this region are considered to belong to the
+    bin closest to that point.
+
+    Public methods:
+        get_bin_num     Return the bin index for a given state
+
+    """
+
+    def __init__(self, low_bound, up_bound, bin_counts):
+        pass
+
+    def get_bin_num(self, coords):
+        pass
