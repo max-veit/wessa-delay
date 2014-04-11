221287b1e9a52a7013187b983c4345079cf4a8e2
Max Veit <maxdveit@gmail.com>
2014-04-11 01:08:13 -0500

    Added util to compute joint delayed probability distribution.
    Also tested; seems to work fine.

--- python/util.py
+++ python/util.py
@@ -10,6 +10,7 @@ No command-line functionality; this module must be imported to be used.
 """
 
 import sys
+import bisect
 
 import numpy as np
 
@@ -42,7 +43,48 @@ def conditional_delay_average(traj, paving, tau, exclude_begin=True):
     """
     pass
 
+def delay_joint_pdist(traj, tau, paving_now, paving_tau, existing=None,
+        from_time=None):
+    """
+    Compute the joint probability distribution for a delayed trajectory.
+
+    Parameters:
+        traj        Trajectory from which to read the history
+        tau         Delay time between the two state measurements
+        paving_now  Paving for the "current" coordinate
+        paving_tau  Paving for the "delayed" coorindate
+
+    Optional Parameters:
+        existing    Existing joint probability distribution; this
+                    function will add its tallies into this array.
+                    Must be a 2-D array, first index is the "current"
+                    coord, second is the "delayed" coord. Obviously, the
+                    shape must match the bin counts for each paving.
+                    Default None, meaning a new array will be created.
+        from_time   Time (on current coordinate) at which to start
+                    tallying. Default None, meaning tally the entire
+                    history beginning at t = tau.
+
+    """
+    if not from_time:
+        from_time = traj.init_time + tau
+    if existing is not None:
+        tallies = existing
+        # TODO Check dimensions?
+    else:
+        tallies = np.zeros((paving_now.num_bins, paving_tau.num_bins))
+    start_idx = bisect.bisect_right(traj.hist_times, from_time)
+    curr_times = traj.hist_times[start_idx:]
+    curr_states = traj.hist_states[start_idx:]
+    delay_times = np.array(curr_times) - tau
+    delay_states = traj.sample_state_seq(delay_times).transpose()
+    for evt_idx in range(len(curr_times)):
+        curr_idx = paving_now.get_bin_num(curr_states[evt_idx])
+        delay_idx = paving_tau.get_bin_num(delay_states[evt_idx])
+        tallies[curr_idx, delay_idx] += 1
+    return tallies
+
 if __name__ == "__main__":
-    print __doc__
+    print(__doc__)
     sys.exit(1)
 
